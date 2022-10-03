4d651477c993d346044ff33f052964788a6200f1
Christian Kolb <info@digital-craftsman.de>
2022-10-03 16:32:23 +0200

    Add doctrine type for time

---
+++ src/Doctrine/TimeType.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigitalCraftsman\DateTimeUtils\Doctrine;
+
+use DigitalCraftsman\DateTimeUtils\ValueObject\Time;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\TimeImmutableType;
+
+final class TimeType extends TimeImmutableType
+{
+    public const NAME = 'digital_craftsman_time';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    /** @param Time|null $value */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        return $value->format($platform->getTimeFormatString());
+    }
+
+    /** @param string|null $value */
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?Time
+    {
+        if ($value === null || $value instanceof Time) {
+            return $value;
+        }
+
+        return Time::fromString($value);
+    }
+}
---
+++ tests/Doctrine/TimeTypeTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigitalCraftsman\DateTimeUtils\Doctrine;
+
+use DigitalCraftsman\DateTimeUtils\ValueObject\Time;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use PHPUnit\Framework\TestCase;
+
+final class TimeTypeTest extends TestCase
+{
+    /** @test */
+    public function convert_from_and_to_time_php_value_works(): void
+    {
+        // -- Arrange
+        $time = new Time(14, 50, 10);
+        $timeType = new TimeType();
+        $platform = new PostgreSQLPlatform();
+
+        // -- Act
+        $databaseValue = $timeType->convertToDatabaseValue($time, $platform);
+        $phpValue = $timeType->convertToPHPValue($databaseValue, $platform);
+
+        // -- Assert
+        self::assertEquals($time, $phpValue);
+    }
+
+    /** @test */
+    public function convert_from_and_to_null_value_works(): void
+    {
+        // -- Arrange
+        $timeType = new TimeType();
+        $platform = new PostgreSQLPlatform();
+
+        // -- Act
+        $databaseValue = $timeType->convertToDatabaseValue(null, $platform);
+        $phpValue = $timeType->convertToPHPValue($databaseValue, $platform);
+
+        // -- Assert
+        self::assertNull($phpValue);
+    }
+}
