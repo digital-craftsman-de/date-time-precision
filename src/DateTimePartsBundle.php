9a1ab1efaf4f6c19109474c635108400b69abea9
Christian Kolb <info@digital-craftsman.de>
2022-10-07 14:52:27 +0200

    Fix renaming issue

--- src/DateTimePartsBundle.php
+++ src/DateTimePartsBundle.php
@@ -5,6 +5,6 @@ namespace DigitalCraftsman\DateTimeParts;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /** @codeCoverageIgnore */
-final class DateTimeUtilsBundle extends Bundle
+final class DateTimePartsBundle extends Bundle
 {
 }
