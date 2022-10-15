290dd1628d257a1e389dcb82c8dc5ef9a601ea5a
Christian Kolb <info@digital-craftsman.de>
2022-10-15 13:46:46 +0200

    Add more time tests

---
+++ tests/ValueObject/TimeIsBeforeOrEqualToTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigitalCraftsman\DateTimeParts\ValueObject;
+
+use PHPUnit\Framework\TestCase;
+
+/** @coversDefaultClass \DigitalCraftsman\DateTimeParts\ValueObject\Time */
+final class TimeIsBeforeOrEqualToTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider dataProvider
+     *
+     * @covers ::isBeforeOrEqualTo
+     */
+    public function is_before_or_equal_to_works(
+        bool $expectedResult,
+        Time $time,
+        Time $comparator,
+    ): void {
+        // -- Act & Assert
+        self::assertSame($expectedResult, $time->isBeforeOrEqualTo($comparator));
+    }
+
+    /**
+     * @return array<string, array{
+     *   0: boolean,
+     *   1: Time,
+     *   2: Time,
+     * }>
+     */
+    public function dataProvider(): array
+    {
+        return [
+            '1 hour after' => [
+                false,
+                Time::fromString('16:00:00'),
+                Time::fromString('15:00:00'),
+            ],
+            'same time' => [
+                true,
+                Time::fromString('15:00:00'),
+                Time::fromString('15:00:00'),
+            ],
+            '1 hour before' => [
+                true,
+                Time::fromString('15:00:00'),
+                Time::fromString('16:00:00'),
+            ],
+            '1 minute before' => [
+                true,
+                Time::fromString('15:00:00'),
+                Time::fromString('15:01:00'),
+            ],
+            '1 second before' => [
+                true,
+                Time::fromString('15:00:00'),
+                Time::fromString('15:00:01'),
+            ],
+            '1 millisecond before' => [
+                true,
+                Time::fromString('15:00:00'),
+                Time::fromString('15:00:00.000001'),
+            ],
+        ];
+    }
+}
