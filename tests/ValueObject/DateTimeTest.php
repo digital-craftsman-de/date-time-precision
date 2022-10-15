e8c7f1c3089205b1cf744d20e15e611a7c36570b
Christian Kolb <info@digital-craftsman.de>
2022-10-15 11:41:58 +0200

    Add coverage for the rest of DateTime

--- tests/ValueObject/DateTimeTest.php
+++ tests/ValueObject/DateTimeTest.php
@@ -19,7 +19,7 @@ final class DateTimeTest extends TestCase
      */
     public function construction_with_date_time_works(): void
     {
-        // -- Assert
+        // -- Arrange
         $dateTimeImmutable = new \DateTimeImmutable('now');
 
         // -- Act
@@ -27,6 +27,48 @@ final class DateTimeTest extends TestCase
 
         // -- Assert
         self::assertSame($dateTimeImmutable, $dateTime->dateTime);
+    }
+
+    /**
+     * @test
+     *
+     * @covers ::fromString
+     */
+    public function from_string_works(): void
+    {
+        // -- Arrange & Act
+        $dateTime = DateTime::fromString('2022-10-08 15:00:00');
+
+        // -- Assert
+        self::assertEquals(new \DateTimeImmutable('2022-10-08 15:00:00'), $dateTime->dateTime);
+    }
+
+    /**
+     * @test
+     *
+     * @covers ::fromStringInTimeZone
+     */
+    public function from_string_in_time_zone_works(): void
+    {
+        // -- Arrange & Act
+        $dateTime = DateTime::fromStringInTimeZone('2022-10-08 15:00:00', new \DateTimeZone('Europe/Berlin'));
+
+        // -- Assert
+        self::assertEquals(new \DateTimeImmutable('2022-10-08 13:00:00'), $dateTime->dateTime);
+    }
+
+    /**
+     * @test
+     *
+     * @covers ::__toString
+     */
+    public function to_string_works(): void
+    {
+        // -- Arrange & Act
+        $dateTime = DateTime::fromStringInTimeZone('2022-10-08 15:00:00', new \DateTimeZone('Europe/Berlin'));
+
+        // -- Assert
+        self::assertEquals('2022-10-08T13:00:00+00:00', (string) $dateTime);
     }
 
     // -- Mutations
