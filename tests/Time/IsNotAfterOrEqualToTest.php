43a31401ff0e0173145e8388deb47f2f970afd70
Christian Kolb <info@digital-craftsman.de>
2024-03-16 22:09:27 +0100

    Add missing time tests

---
+++ tests/Time/IsNotAfterOrEqualToTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigitalCraftsman\DateTimePrecision\Time;
+
+use DigitalCraftsman\DateTimePrecision\Time;
+use PHPUnit\Framework\TestCase;
+
+/** @coversDefaultClass \DigitalCraftsman\DateTimePrecision\Time */
+final class IsNotAfterOrEqualToTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider dataProvider
+     *
+     * @covers ::isNotAfterOrEqualTo
+     */
+    public function is_not_after_or_equal_to_works(
+        bool $expectedResult,
+        Time $time,
+        Time $comparator,
+    ): void {
+        // -- Act & Assert
+        self::assertSame($expectedResult, $time->isNotAfterOrEqualTo($comparator));
+    }
+
+    /**
+     * @return array<string, array{
+     *   0: boolean,
+     *   1: Time,
+     *   2: Time,
+     * }>
+     */
+    public static function dataProvider(): array
+    {
+        return [
+            '1 hour before' => [
+                true,
+                Time::fromString('15:00:00'),
+                Time::fromString('16:00:00'),
+            ],
+            'same time' => [
+                false,
+                Time::fromString('15:00:00'),
+                Time::fromString('15:00:00'),
+            ],
+            '1 hour later' => [
+                false,
+                Time::fromString('16:00:00'),
+                Time::fromString('15:00:00'),
+            ],
+            '1 minute later' => [
+                false,
+                Time::fromString('15:01:00'),
+                Time::fromString('15:00:00'),
+            ],
+            '1 second later' => [
+                false,
+                Time::fromString('15:00:01'),
+                Time::fromString('15:00:00'),
+            ],
+            '1 millisecond later' => [
+                false,
+                Time::fromString('15:00:00.000001'),
+                Time::fromString('15:00:00'),
+            ],
+        ];
+    }
+}
---
+++ tests/Time/IsNotBeforeTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigitalCraftsman\DateTimePrecision\Time;
+
+use DigitalCraftsman\DateTimePrecision\Time;
+use PHPUnit\Framework\TestCase;
+
+/** @coversDefaultClass \DigitalCraftsman\DateTimePrecision\Time */
+final class IsNotBeforeTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider dataProvider
+     *
+     * @covers ::isNotBefore
+     */
+    public function is_not_before_works(
+        bool $expectedResult,
+        Time $time,
+        Time $comparator,
+    ): void {
+        // -- Act & Assert
+        self::assertSame($expectedResult, $time->isNotBefore($comparator));
+    }
+
+    /**
+     * @return array<string, array{
+     *   0: boolean,
+     *   1: Time,
+     *   2: Time,
+     * }>
+     */
+    public static function dataProvider(): array
+    {
+        return [
+            '1 hour after' => [
+                true,
+                Time::fromString('16:00:00'),
+                Time::fromString('15:00:00'),
+            ],
+            'same time' => [
+                true,
+                Time::fromString('15:00:00'),
+                Time::fromString('15:00:00'),
+            ],
+            '1 hour before' => [
+                false,
+                Time::fromString('15:00:00'),
+                Time::fromString('16:00:00'),
+            ],
+            '1 minute before' => [
+                false,
+                Time::fromString('15:00:00'),
+                Time::fromString('15:01:00'),
+            ],
+            '1 second before' => [
+                false,
+                Time::fromString('15:00:00'),
+                Time::fromString('15:00:01'),
+            ],
+            '1 millisecond before' => [
+                false,
+                Time::fromString('15:00:00'),
+                Time::fromString('15:00:00.000001'),
+            ],
+        ];
+    }
+}
