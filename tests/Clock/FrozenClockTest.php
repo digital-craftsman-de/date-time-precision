f34b09ab995ada9eb402fcbbe3c7d05967e1ac8e
Christian Kolb <info@digital-craftsman.de>
2024-05-06 08:32:10 +0200

    Extend tests to cover mutants

--- tests/Clock/FrozenClockTest.php
+++ tests/Clock/FrozenClockTest.php
@@ -13,6 +13,19 @@ use PHPUnit\Framework\TestCase;
 final class FrozenClockTest extends TestCase
 {
     #[Test]
+    public function construction_works(): void
+    {
+        // -- Arrange
+        $moment = Moment::fromStringInTimeZone('2024-05-06 08:00:00', new \DateTimeZone('Europe/Berlin'));
+
+        // -- Act
+        $clock = new FrozenClock($moment);
+
+        // -- Assert
+        self::assertSame($moment, $clock->now());
+    }
+
+    #[Test]
     public function freeze_works(): void
     {
         // -- Arrange
