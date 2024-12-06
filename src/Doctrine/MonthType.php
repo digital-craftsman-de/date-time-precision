771f96356a089b3e1f8e3766e200860790a513bf
Christian Kolb <info@digital-craftsman.de>
2024-12-06 16:41:09 +0100

    Refactor month

--- src/Doctrine/MonthType.php
+++ src/Doctrine/MonthType.php
@@ -5,46 +5,28 @@ declare(strict_types=1);
 namespace DigitalCraftsman\DateTimePrecision\Doctrine;
 
 use DigitalCraftsman\DateTimePrecision\Month;
+use DigitalCraftsman\SelfAwareNormalizers\Doctrine\StringNormalizableType;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Types\StringType;
 
-final class MonthType extends StringType
+/**
+ * @codeCoverageIgnore
+ */
+final class MonthType extends StringNormalizableType
 {
-    /** @codeCoverageIgnore */
-    public function getName(): string
+    public static function getTypeName(): string
     {
         return 'dtp_month';
     }
 
-    /** @param Month|null $value */
-    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    public static function getClass(): string
     {
-        if ($value === null) {
-            return null;
-        }
-
-        return (string) $value;
+        return Month::class;
     }
 
-    /** @param string|null $value */
-    public function convertToPHPValue($value, AbstractPlatform $platform): ?Month
-    {
-        return $value === null
-            ? null
-            : Month::fromString($value);
-    }
-
-    /** @codeCoverageIgnore */
     public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
     {
         $column['length'] = 7;
 
         return $platform->getStringTypeDeclarationSQL($column);
-    }
-
-    /** @codeCoverageIgnore */
-    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
-    {
-        return true;
     }
 }
