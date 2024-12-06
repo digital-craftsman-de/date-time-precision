24f0bc9da69376820fa865c7e13f0f7075bbcc97
Christian Kolb <info@digital-craftsman.de>
2024-12-06 16:23:59 +0100

    Switch Date

--- src/Doctrine/DateType.php
+++ src/Doctrine/DateType.php
@@ -5,44 +5,26 @@ declare(strict_types=1);
 namespace DigitalCraftsman\DateTimePrecision\Doctrine;
 
 use DigitalCraftsman\DateTimePrecision\Date;
+use DigitalCraftsman\SelfAwareNormalizers\Doctrine\StringNormalizableType;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Types\Type;
 
-final class DateType extends Type
+/**
+ * @codeCoverageIgnore
+ */
+final class DateType extends StringNormalizableType
 {
-    /** @codeCoverageIgnore */
-    public function getName(): string
+    public static function getTypeName(): string
     {
         return 'dtp_date';
     }
 
-    /** @codeCoverageIgnore */
+    public static function getClass(): string
+    {
+        return Date::class;
+    }
+
     public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
     {
         return $platform->getDateTypeDeclarationSQL($column);
-    }
-
-    /** @param Date|null $value */
-    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
-    {
-        if ($value === null) {
-            return null;
-        }
-
-        return $value->format($platform->getDateFormatString());
-    }
-
-    /** @param string|null $value */
-    public function convertToPHPValue($value, AbstractPlatform $platform): ?Date
-    {
-        return $value === null
-            ? null
-            : Date::fromString($value);
-    }
-
-    /** @codeCoverageIgnore */
-    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
-    {
-        return true;
     }
 }
